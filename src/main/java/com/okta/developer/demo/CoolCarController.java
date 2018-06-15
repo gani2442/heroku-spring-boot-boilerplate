a7d6ff65fd2655a44d5a51c28ed42d1704de282a
gani2442 <ganesh-p@hcl.com>
2018-06-15 20:24:57 +0530

    added car chnages

--- src/main/java/com/okta/developer/demo/CoolCarController.java
+++ src/main/java/com/okta/developer/demo/CoolCarController.java
@@ -1,5 +1,4 @@
 package com.okta.developer.demo;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +14,13 @@ class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
-    @CrossOrigin(origins = "https://still-ravine-79015.herokuapp.com")
+    @CrossOrigin(origins = "https://ganesh-angular-schb.herokuapp.com")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
                 .collect(Collectors.toList());
     }
+
 
     private boolean isCool(Car car) {
         return !car.getName().equals("AMC Gremlin") &&
